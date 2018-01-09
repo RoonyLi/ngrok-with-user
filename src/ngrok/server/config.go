7f8e67ea594ef7a5f2979a1e1dbd012cf628efdb
RoonyLi <lizhaoedu@gmail.com>
2018-01-09 15:03:26 +0800

    修改用户逻辑

--- src/ngrok/server/config.go
+++ src/ngrok/server/config.go
@@ -25,7 +25,7 @@ const (
 type UserConfig struct {
 	User string   `json:"user"`
 	Password string   `json:"password"`
-	Tunnel []*ClientTunnel   `json:"tunnel"`
+	Tunnel []*msg.ClientTunnel   `json:"tunnel"`
 }
 
 type UserInfo struct {

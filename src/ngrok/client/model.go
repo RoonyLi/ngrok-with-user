a863f1943653b6c73d8f8eb4a170dd08bd632431
RoonyLi <lizhaoedu@gmail.com>
2018-03-05 18:14:23 +0800

    default domain

--- src/ngrok/client/model.go
+++ src/ngrok/client/model.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	defaultServerAddr   = "tunnel.bestngrok.top:4545"
+	defaultServerAddr   = "tunnel.lzxianren.com:4545"
 	defaultInspectAddr  = "127.0.0.1:4040"
 	pingInterval        = 20 * time.Second
 	maxPongLatency      = 15 * time.Second
